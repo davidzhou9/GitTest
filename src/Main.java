74ba035a7b82c251827f796e6a3356c185bc73e2
davidzhou9 <dz4@princeton.edu>
2016-11-07 14:14:55 -0500

    added subtract method

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
         add(2, 3);
     }
 
+    // function to print out the sum of two ints
     public static void add(int x, int y) {
-        System.out.println(x + y +"");
+        System.out.println(x + y + "");
+    }
+
+    // function to print out the difference of two ints
+    public static void subtract(int x, int y) {
+        System.out.println(x - y + "");
     }
 }
