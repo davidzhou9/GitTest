37bffb7545fd0911a4a7a02de822a7967c645f71
davidzhou9 <dz4@princeton.edu>
2016-11-07 14:08:13 -0500

    Added the add function to sum 2 given ints

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     // Main method to print "Hello World!"
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        add(2, 3);
+    }
+
+    public static void add(int x, int y) {
+        System.out.println(x + y +"");
     }
 }
